94b0f2d1bbfdde950bd3b7a9ad9a424be03e3116
maryana-frydrak <maryanafrydrak@gmail.com>
2026-06-24 18:03:41 +0300

    Finish server implementation and middleware

--- src/server.js
+++ src/server.js
@@ -1,9 +1,45 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(pino());
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  next;
+  res.status(500).json({
+    message: err.message,
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
