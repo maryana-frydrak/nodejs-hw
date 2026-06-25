eba8e743a03d2153beb587c92fa2e18e921c07a7
maryana-frydrak <maryanafrydrak@gmail.com>
2026-06-25 23:30:59 +0300

    add a structure, work with middleware, connectMongoDB & server.js

--- src/server.js
+++ src/server.js
@@ -2,11 +2,17 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import { connectMongoDB } from './db/connectMongoDB';
+import { logger } from './middleware/logger';
+import { notFoundHandler } from './middleware/notFoundHandler';
+import { errorHandler } from './middleware/errorHandler';
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(logger);
 app.use(express.json());
 app.use(cors());
 app.use(pino());
@@ -24,16 +30,13 @@ app.get('/notes/:noteId', (req, res) => {
   });
 });
 
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
-
 app.use((req, res) => {
   res.status(404).json({
     message: 'Route not found',
   });
 });
 
+/* eslint-disable no-unused-vars */
 app.use((err, req, res, next) => {
   res.status(500).json({
     message: 'Server Error',
@@ -41,7 +44,11 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
