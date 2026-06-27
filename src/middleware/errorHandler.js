5b6bb093974a59b3ddc6489ba86d1dc27370481f
maryana-frydrak <maryanafrydrak@gmail.com>
2026-06-27 20:35:10 +0300

    fix errorHandler.js

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -4,16 +4,12 @@ import { HttpError } from 'http-errors';
 export const errorHandler = (err, req, res, next) => {
   if (err instanceof HttpError) {
     res.status(err.status).json({
-      status: err.status,
-      message: err.name,
-      data: err.message,
+      message: err.message,
     });
     return;
   }
 
   res.status(500).json({
-    status: 500,
-    message: 'Something went wrong',
-    data: err.message,
+    message: err.message,
   });
 };
