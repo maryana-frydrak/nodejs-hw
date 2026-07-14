3b542c64b24c7eba48387bcaef8205c6a6695ce1
Mariana Frydrak <maryanafrydrak@gmail.com>
2026-07-14 21:07:35 +0300

    registerUser

---
+++ src/controllers/authController.js
@@ -0,0 +1,26 @@
+import createHttpError from 'http-errors';
+import { User } from '../models/user.js';
+import bcrypt from 'bcrypt';
+import { createSession, setSessionCookies } from '../services/auth.js';
+
+export const registerUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw createHttpError(400, 'Email in use');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    email,
+    password: hashedPassword,
+  });
+
+  const newSession = await createSession(user._id);
+
+  setSessionCookies(res, newSession);
+
+  res.status(201).json(user);
+};
--- src/server.js
+++ src/server.js
@@ -5,8 +5,10 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -16,7 +18,9 @@ const PORT = process.env.PORT || 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
 app.use(errors());
