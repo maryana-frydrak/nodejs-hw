9f51bd64a7fae2573778ce8586743ff3aa84f4ed
Mariana Frydrak <maryanafrydrak@gmail.com>
2026-07-14 22:55:26 +0300

    login, logout, authenticate & update notes

--- src/models/note.js
+++ src/models/note.js
@@ -18,10 +18,15 @@ const noteSchema = new Schema(
       enum: TAGS,
       default: 'Todo',
     },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
   { timestamps: true, versionKey: false },
 );
 
-noteSchema.index({ tag: 1 });
+noteSchema.index({ tag: 1, userId: 1 });
 
 export const Note = model('Note', noteSchema);
